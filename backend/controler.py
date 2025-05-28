02afb96327015634ba85011f3dbf7cd948364c38
spideryzarc <spideryzarc@gmail.com>
2025-05-28 18:16:15 -0300

    Enhance depot management: add geocoding for new depots, implement toggle for active status, and update depot details with latitude and longitude

--- backend/controler.py
+++ backend/controler.py
@@ -1,4 +1,5 @@
 from backend.model import Session, Depots
+from osmnx import geocode 
 
 def get_depots():
     """
@@ -10,13 +11,52 @@ def get_depots():
     return depots
 
 def add_depot(name, address):
-    """
+    """p
     Add a new depot.
     """
     session = Session()
     depot = Depots(name=name, address=address)
+    #search for latitude and longitude using OSMN
+    if address:
+        location = geocode(address)
+        print(f"Location found for {address}: {location}")
+        if location:
+            depot.latitude = location[0]
+            depot.longitude = location[1]
+    else:
+        depot.latitude = None
+        depot.longitude = None
+
+
     session.add(depot)
     session.commit()
+    session.close()
+    return depot
+
+def toggle_depot_active(depot_id, active):
+    """
+    Toggle the active status of a depot.
+    """
+    session = Session()
+    depot = session.query(Depots).filter(Depots.id == depot_id).first()
+    if depot:
+        depot.active = active
+        session.commit()
+    session.close()
+    return depot
+
+def update_depot(depot_id, new_name, new_address, new_latitude=None, new_longitude=None):
+    """
+    Update an existing depot.
+    """
+    session = Session()
+    depot = session.query(Depots).filter(Depots.id == depot_id).first()
+    if depot:
+        depot.name = new_name
+        depot.address = new_address
+        depot.latitude = new_latitude
+        depot.longitude = new_longitude
+        session.commit()
     session.close()
     return depot
 
--- frontend/pages.py
+++ frontend/pages.py
@@ -39,12 +39,8 @@ def depots():
             with ui.card().classes('w-full'):
                 ui.label("Depósitos Cadastrados").classes("text-h5")                
                 ui.button("Adicionar", on_click=open_add_depot_card, color="primary", icon="add").classes("mb-4")
-                # Toggle para mostrar depósitos desativados
-                show_disabled = ui.checkbox("Mostrar depósitos desativados", value=False)
-                ui.separator()	
-                # Container para a lista de depósitos, que será atualizado dinamicamente
-                deposits_container = ui.column().classes('w-full')
 
+                # Container para a lista de depósitos, que será atualizado dinamicamente
                 def refresh_depositos():
                     deposits_container.clear()
                     depositos = get_depots()  # Função que retorna os depósitos
@@ -52,15 +48,23 @@ def depots():
                         for depot in depositos:
                             if depot.active or show_disabled.value:
                                 with deposits_container:
-                                    with ui.card().classes('w-full mb-2'): # Adiciona margem inferior aos cards
+                                    with ui.row().classes('items-center justify-between w-full'):
                                         ui.label(f"({depot.id}) {depot.name}").classes("text-h6")
-                                        ui.label(f"Endereço: {depot.address}")
-                                        with ui.card_actions().classes('w-full justify-end'):
-                                            ui.button("Editar", on_click=lambda d=depot: open_edit_depot(d), color="primary", icon="edit")
-                                            ui.button("Desativar", on_click=lambda d=depot: open_disable_confirm(d), color="warning", icon="delete")
+                                        # ui.label(f"Endereço: {depot.address}")
+                                        with ui.row().classes('items-center gap-2'):
+                                            ui.button("", on_click=lambda d=depot: open_edit_depot(d), color="primary", icon="edit")
+                                            if depot.active:
+                                                ui.button("", on_click=lambda d=depot: confirm_active_toggle(d,False), color="warning", icon="delete")
+                                            else:
+                                                ui.button("", on_click=lambda d=depot: confirm_active_toggle(d, True), color="success", icon="check")
                     else:
                         with deposits_container:
                             ui.label("Nenhum depósito encontrado.")
+                
+                # Toggle para mostrar depósitos desativados
+                show_disabled = ui.checkbox("Mostrar depósitos desativados", value=False, on_change=lambda: refresh_depositos()).classes('mb-4')
+                ui.separator()	
+                deposits_container = ui.column().classes('w-full')
 
                 # Atualiza a lista sempre que o checkbox for modificado
                 show_disabled.on("update", lambda: refresh_depositos())
@@ -69,29 +73,54 @@ def depots():
         # Coluna da direita: Mapa ocupando o espaço restante
         with ui.column().classes('flex-grow p-4'): # flex-grow faz a coluna ocupar o espaço restante
             with ui.card().classes('w-full h-full'): # Card ocupa todo o espaço da coluna
-                # Cria o mapa centralizado em São Paulo
-                m = folium.Map(location=[-23.55052, -46.633308], zoom_start=12)
-                # Exemplo: adiciona um marcador
-                folium.Marker(location=[-23.55052, -46.633308], popup="Centro de São Paulo").add_to(m)
+                #lista de depósitos para exibir no mapa
+                depots = get_depots()
+                coords = [ (depot.latitude, depot.longitude) for depot in depots if depot.latitude and depot.longitude]
+                # Cria o mapa centralizado na média das coordenadas dos depósitos
+                if coords:
+                    avg_lat = sum(lat for lat, lon in coords) / len(coords)
+                    avg_lon = sum(lon for lat, lon in coords) / len(coords)
+                    m = folium.Map(location=[avg_lat, avg_lon], zoom_start=12)
+                    # Adiciona marcadores para cada depósito
+                    for depot in depots:
+                        if depot.latitude and depot.longitude:
+                            folium.Marker(
+                                location=[depot.latitude, depot.longitude],
+                                popup=f"({depot.id}) {depot.name}<br>{depot.address}",
+                                icon=folium.Icon(color='blue' if depot.active else 'gray')
+                            ).add_to(m)
+                else:
+                    # Se não houver depósitos com coordenadas, centraliza em São Paulo
+                    m = folium.Map(location=[-23.55052, -46.633308], zoom_start=12)
+                    ui.label("Nenhum depósito com coordenadas disponíveis. Exibindo mapa de São Paulo.")
+                    
+                # # Cria o mapa centralizado em São Paulo
+                # m = folium.Map(location=[-23.55052, -46.633308], zoom_start=12)
+                # # Exemplo: adiciona um marcador
+                # folium.Marker(location=[-23.55052, -46.633308], popup="Centro de São Paulo").add_to(m)
                 # Obtém a representação HTML do mapa
                 map_html = m._repr_html_()
                 # Exibe o mapa na interface, fazendo-o ocupar todo o card
                 ui.html(map_html).classes('w-full h-full')
 
 # Modal de confirmação para desativar um depósito
-def open_disable_confirm(depot):
+def confirm_active_toggle(depot, active):
+    action = "ativar" if active else "desativar"
+    def confirm_disable(dialog, depot):
+        toggle_depot_active(depot.id, active)  # Implemente essa função para desativar o depósito
+        ui.notify("Depósito atualizado!")
+        dialog.close()
+        # reload a página para atualizar a lista de depósitos
+        ui.navigate.to('/depots')
     with ui.dialog() as confirm_dialog, ui.card():
-        ui.label("Deseja desativar o depósito:").classes("text-h5")
+        ui.label(f"Deseja {action} o depósito:").classes("text-h5")
         ui.label(f"({depot.id}) {depot.name}").classes("text-h6")
         with ui.card_actions().classes('w-full justify-end'):
             ui.button("Sim", on_click=lambda: confirm_disable(confirm_dialog, depot), color="warning", icon="delete")
             ui.button("Não", on_click=confirm_dialog.close, icon="close")
     confirm_dialog.open()
 
-def confirm_disable(dialog, depot):
-    disable_depot(depot)
-    ui.notify("Depósito desativado!")
-    dialog.close()
+
 
 # Exemplo de uso dentro de um dialog com card (para adicionar depósito):
 def open_add_depot_card():
@@ -108,18 +137,29 @@ def save_and_close(dialog, name, address):
     add_depot(name, address)
     ui.notify("Depósito adicionado com sucesso!")
     dialog.close()
+    # reload a página para atualizar a lista de depósitos
+    ui.navigate.to('/depots')
 
 def open_edit_depot(depot):
     with ui.dialog() as edit_dialog, ui.card():
         ui.label("Editar Depósito").classes("text-h6")
         name_input = ui.input(label="Nome", value=depot.name)
         address_input = ui.input(label="Endereço", value=depot.address)
+        lat_input = ui.input(label="Latitude", value=depot.latitude if depot.latitude else "")
+        lon_input = ui.input(label="Longitude", value=depot.longitude if depot.longitude else "")
         with ui.card_actions().classes('w-full justify-end'):
-            ui.button("Salvar", on_click=lambda: save_edit_depot(edit_dialog, depot, name_input.value, address_input.value), color="primary", icon="save")
+            ui.button("Salvar", on_click=lambda: save_edit_depot(edit_dialog, depot, 
+                                                                 name_input.value, 
+                                                                 address_input.value,
+                                                                 lat_input.value,
+                                                                 lon_input.value
+                                                                 ), color="primary", icon="save")
             ui.button("Cancelar", on_click=edit_dialog.close, color="negative", icon="close")
     edit_dialog.open()
 
-def save_edit_depot(dialog, depot, new_name, new_address):
-    update_depot(depot, new_name, new_address)  # Implemente essa função para atualizar o depósito
+def save_edit_depot(dialog, depot, new_name, new_address, new_latitude=None, new_longitude=None):
+    update_depot(depot.id, new_name, new_address, new_latitude, new_longitude)
     ui.notify("Depósito atualizado com sucesso!")
     dialog.close()
+    # reload a página para atualizar a lista de depósitos
+    ui.navigate.to('/depots')
